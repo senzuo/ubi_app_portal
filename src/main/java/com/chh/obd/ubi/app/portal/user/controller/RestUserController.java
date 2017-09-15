bda6f83d4d24b2686eb58777987434149ff166c5
senzuo <senzuo@hotmail.com>
2017-09-15 17:31:29 +0800

    init

--- src/main/java/com/chh/obd/ubi/app/portal/user/controller/RestUserController.java
+++ src/main/java/com/chh/obd/ubi/app/portal/user/controller/RestUserController.java
@@ -2,24 +2,18 @@ package com.chh.obd.ubi.app.portal.user.controller;
 
 import com.chh.obd.ubi.app.portal.common.response.RestResponse;
 import com.chh.obd.ubi.app.portal.common.response.RestUtil;
-import com.chh.obd.ubi.app.portal.user.model.User;
-import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by 申卓 on 2017/9/15.
  */
 
-@Controller
+@RestController
 public class RestUserController {
     @RequestMapping(value="/login", method = RequestMethod.GET)
-    @ResponseBody
     @ApiOperation(value = "登录", httpMethod = "GET", response = RestUserController.class, notes = "接收user对象登录")
     public RestResponse login(){
         RestResponse restResponse = RestUtil.getResponse();
